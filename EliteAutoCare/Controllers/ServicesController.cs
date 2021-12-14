74340b38e03b5d920fe76f09a2f576d915b627a7
Kübra SELÇUK <77327246+Kkubraselcuk@users.noreply.github.com>
2021-12-14 10:42:22 +0300

    Add project files.

---
+++ EliteAutoCare/Controllers/ServicesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EliteAutoCare.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CeramicCoating() //Seramik kaplama
+        {
+            return View();
+        }
+        public IActionResult ExteriorDetailing() //Dış Detaylandırma
+        {
+            return View();
+        }
+        public IActionResult InteriorDetailing() //İç Detaylandırma
+        {
+            return View();
+        }
+        public IActionResult PaintCorrection() //Boya Düzeltme
+        {
+            return View();
+        }
+        public IActionResult CompleteDetailing() //Tam Detaylandırma
+        {
+            return View();
+        }
+        public IActionResult MaintenanceDetailing() //Detaylı Bakım
+        {
+            return View();
+        }
+        public IActionResult HeadlampRestoration() //Far
+        {
+            return View();
+        }
+    }
+}
