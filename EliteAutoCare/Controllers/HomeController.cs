85276e3d54eb1f946d19b1c70b607e2d8d8a186b
Kübra SELÇUK <77327246+Kkubraselcuk@users.noreply.github.com>
2021-12-21 13:07:53 +0300

    Mail işlemleri

--- EliteAutoCare/Controllers/HomeController.cs
+++ EliteAutoCare/Controllers/HomeController.cs
@@ -5,8 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
-
 namespace EliteAutoCare.Controllers
 {
     public class HomeController : Controller
@@ -22,12 +23,40 @@ namespace EliteAutoCare.Controllers
         {
             return View();
         }
-        
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Contact(Mail m)  //Mail sınıfından m diye bir değişken tanımlarız
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Burası aynı kalacak
+                client.Credentials = new NetworkCredential("kkubra.selcuk@gmail.com", "2534kubrA");
+                client.EnableSsl = true;
+                MailMessage msj = new MailMessage(); //Yeni bir MailMesajı oluşturuyoruz
+                msj.From = new MailAddress(m.Email, m.Adi + " " + m.Soyadi); //iletişim kısmında girilecek mail buaraya gelecektir
+                msj.To.Add("kkubra.selcuk@gmail.com"); //Buraya kendi mail adresimizi yazıyoruz
+                msj.Subject = m.Konu + "     " + m.Email; //Buraya iletişim sayfasında gelecek konu ve mail adresini mail içeriğine yazacaktır
+                msj.Body = m.Mesaj; //Mail içeriği burada aktarılacakır
+                client.Send(msj); //Clien sent kısmında gönderme işlemi gerçeklecektir.
+                //Bu kısımdan itibaren sizden kullanıcıya gidecek mail bilgisidir 
+                MailMessage msj1 = new MailMessage();
+                msj1.From = new MailAddress("kkubra.selcuk@gmail.com", "elite auto care - Sakarya");
+                msj1.To.Add(m.Email); //Buraua iletişim sayfasında gelecek mail adresi gelecktir.
+                msj1.Subject = "Mail'inize Cevap";
+                msj1.Body = "Size en kısa zamanda döneceğiz. Bizi tercih ettiğiniz için teşekkür ederiz ";
+                client.Send(msj1);
+                ViewBag.Succes = "Teşekkürler mailiniz başarılı bir şekilde gönderildi"; //Bu kısımlarda ise kullanıcıya bilgi vermek amacı ile olur
+                return View();
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Mesaj gönderilirken hata olıuştu"; //Bu kısımlarda ise kullanıcıya bilgi vermek amacı ile olur
+                return View();
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
